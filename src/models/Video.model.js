675f1b346c07568873b14b3afeec3c497345ddcb
Shashwat-Trivedi <shashwatrivedi2005@gmail.com>
2025-09-01 17:00:40 +0530

    Video and User models

---
+++ src/models/Video.model.js
@@ -0,0 +1,48 @@
+import mongoose, {Schema} from "mongoose";
+
+const videoSchema =new Schema(
+    {
+        videoFile: {
+            type: String, //cloudniary url
+            required: true
+        },
+
+        thumbnail: {
+            type: String, //cloudniary url
+            required: true
+        },
+
+        title: {
+            type: String, 
+            required: true
+        },
+
+        discription: {
+            type: String, //cloudniary url
+            required: true
+        },
+
+        duration: {
+            type: Number,
+            required: true
+        },
+
+        views: {
+            type: Number,
+            default: 0
+        },
+
+        isPublished: {
+            type: Boolean,
+            daeault: True
+        },
+
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        }
+
+    },
+    {timestamps: true}
+)
